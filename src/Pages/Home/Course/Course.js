7bc299fe2b3f41c41ca9762f6600b92de7878833
Mahmudul Kabir <mahmudulkabir990@gmail.com>
2022-04-18 03:38:18 +0600

    courses added

---
+++ src/Pages/Home/Course/Course.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Course = ({ course }) => {
+    const { img, subject, courseDetail, fees } = course;
+    return (
+        <div className='border rounded-3 border-secondary w-100 w-lg-50 p-2'>
+            <img className='w-100' src={img} alt="" />
+            <h3 className='mt-2'>{subject}</h3>
+            <h5>Fees: ${fees}</h5>
+            <p>{courseDetail}</p>
+            <Link to='/checkout' className="btn btn-secondary ">Buy Course</Link>
+        </div>
+    );
+};
+
+export default Course;
---
+++ src/Pages/Home/Courses/Courses.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { CardGroup } from 'react-bootstrap';
+import Course from '../Course/Course';
+
+const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetch('courses.json')
+            .then(res => res.json())
+            .then(data => setCourses(data));
+    }, [])
+    return (
+        <div className='container mb-3'>
+            <h2 className="mt-5 text-center">See My Running Courses</h2>
+            <div className='d-flex flex-column flex-lg-row gap-5 justify-content-center mt-3'>
+                {
+                    courses.map(course => <Course
+                        key={course.id}
+                        course={course}
+                    ></Course>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Courses;
